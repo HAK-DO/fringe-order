adae67f104ad10058d320095a246de41c4660216
LEE HAK DO <future.fringe@gmail.com>
2013-11-13 10:58:20 +0900

    Signed-off-by: LEE HAK DO <future.fringe@gmail.com>

--- Order/src/com/test/orderapp/runner/TestJms.java
+++ Order/src/com/test/orderapp/runner/TestJms.java
@@ -1,16 +1,24 @@
 package com.test.orderapp.runner;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import com.test.orderapp.producer.OrderService;
 
 /**
  * @author hdlee
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:application-context.xml")
 public class TestJms {
-	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
-		OrderService orderService = (OrderService) ctx.getBean("orderService");
+
+	@Autowired OrderService orderService;
+	
+	@Test
+	public void sendReceive() {
 		for(int i =1; i<=5; i++){
 			orderService.sendOrder(1+i, 10.0D+i);
 		}
